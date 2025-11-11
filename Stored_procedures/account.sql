b8b004ea1d2e2ebdc69304a4c0499c5b871c1506
Mark Gonzales Nielsen <98944353+MarkGonzalesNielsen@users.noreply.github.com>
2025-11-11 15:06:55 +0100

    Refactor stored procedures in account.sql
    Removed commented-out calls and adjusted formatting.

--- Stored_procedures/account.sql
+++ Stored_procedures/account.sql
@@ -1,4 +1,8 @@
-//CALL CreateAccount(5, 'Savings Account', 10000000.0);
+// setup: 1. USE dit_database_navn;
+
+//2. copy paste stored functions ind 
+
+DELIMITER //
 
 CREATE PROCEDURE CreateAccount(
     IN p_user_id INT,
@@ -8,25 +12,20 @@ CREATE PROCEDURE CreateAccount(
 BEGIN
     INSERT INTO Account (user_id, name, saldo)
     VALUES (p_user_id, p_name, p_saldo);
-END 
-
-//CALL GetAllAccounts();
-
+END //
 
 CREATE PROCEDURE GetAllAccounts()
 BEGIN
     SELECT idAccount, name, saldo
-    from Account;
-END 
+    FROM Account;
+END //
 
 CREATE PROCEDURE GetAccountById(IN p_id INT)
 BEGIN
     SELECT idAccount, User_idUser, name, saldo
     FROM Account
     WHERE idAccount = p_id;
-END
-
-//CALL UpdateAccount(1, 'Updated Account Name', 1500.0);
+END //
 
 CREATE PROCEDURE UpdateAccount(
     IN p_account_id INT,
@@ -38,10 +37,21 @@ BEGIN
     SET name = p_name,
         saldo = p_saldo
     WHERE idAccount = p_account_id;
-END
+END //
 
 CREATE PROCEDURE DeleteAccount(IN p_account_id INT)
 BEGIN
     DELETE FROM Account WHERE idAccount = p_account_id;
-END
+END //
+
+DELIMITER ;
+
+//3. test at de virker
+CALL CreateAccount(5, 'Savings Account', 10000000.0);
+CALL GetAllAccounts();
+CALL GetAccountById(1);
+CALL UpdateAccount(1, 'Updated Account', 1500.0);
+CALL DeleteAccount(1);
+
+
 
